0c2cdc7a77d4577dc51acde8f677a5ca637c06b6
elliewalkersm <elliewalkersm@gmail.com>
2021-01-23 11:08:31 -0600

    callback challenge

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("It worked");
+// console.log("It worked");
+
+function numberOne(fact) {
+  return 'My name is ellie'
+}
+
+function numberTwo(fact) {
+  return 'I love dogs'
+}
+
+function numberThree(fact, cb) {
+  return cb(fact)
+}
+
+console.log(numberThree('fun fact:', numberTwo))
