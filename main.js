8fd7d047f5504a3fae4e1a0ed15e31b32665b24f
elliewalkersm <elliewalkersm@gmail.com>
2021-01-23 11:59:00 -0600

    worked with click event listener

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("It worked");
+// console.log("It worked");
+
+const button = document.querySelector('#main-btn');
+const container = document.querySelector('#container');
+
+//e stands for event
+// button.addEventListener('click', (e) => {
+//   console.log('you clicked on me');
+//   console.log(e);
+// })
+
+const whatIsTheId = (e) => {
+  console.log(`you clicked ${e.target.id}`);
+
+  if (e.target.id === 'main-btn') {
+    container.innerHTML = 'you clicked the main button';
+  } else {
+    container.innerHTML = 'you clicked the other button'
+  }
+}
+
+// Main Button
+button.addEventListener('click', whatIsTheId);
+
+// Other Button
+const buttonTwo = document.querySelector('#other-btn');
+
+buttonTwo.addEventListener('click', whatIsTheId);
