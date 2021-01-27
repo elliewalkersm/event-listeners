af8efcff3c9d7b47d2667e628eeec768747998e5
elliewalkersm <elliewalkersm@gmail.com>
2021-01-26 21:52:44 -0600

    for in loop

--- main.js
+++ main.js
@@ -84,21 +84,36 @@ const pieBuilder = (taco) => {
   //     <button type="button" class="btn btn-danger" id="${i}">Delete</button>
   //   </div>
   // </div>`;
-  
+
 //forEach Method//
-  taco.forEach((item, i) => {
+  // taco.forEach((item, i) => {
+  //   domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+  //      <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+  //      <div class="card-body">
+  //        <p class="card-text">${item.name}</p>
+  //        <p class="card-text">${item.ingredients}</p>
+  //        <p class="card-text">${item.bakeTemp}</p>
+  //        <p class="card-text">${item.drinkPairing}</p>
+  //        <p class="card-text">${item.iceCream}</p>
+  //        <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+  //      </div>
+  //    </div>`
+  // })
+
+  //FOR IN LOOP//
+  for (const [i, element] of taco.entries()) {
     domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
-       <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
-       <div class="card-body">
-         <p class="card-text">${item.name}</p>
-         <p class="card-text">${item.ingredients}</p>
-         <p class="card-text">${item.bakeTemp}</p>
-         <p class="card-text">${item.drinkPairing}</p>
-         <p class="card-text">${item.iceCream}</p>
-         <button type="button" class="btn btn-danger" id="${i}">Delete</button>
-       </div>
-     </div>`
-  })
+  <div class="img-container" style="background-image: url('${element.imageUrl}');"></div>
+  <div class="card-body">
+    <p class="card-text">${element.name}</p>
+    <p class="card-text">${element.ingredients}</p>
+    <p class="card-text">${element.bakeTemp}</p>
+    <p class="card-text">${element.drinkPairing}</p>
+    <p class="card-text">${element.iceCream}</p>
+    <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+  </div>
+</div>`
+  }
 
 
   printToDom('#pies', domString);
